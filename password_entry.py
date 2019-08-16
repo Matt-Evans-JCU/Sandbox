8d38041de413fe690b9f6c94904690a6eb57f101
Matt <matt.evans@my.jcu.edu.au>
2019-08-16 21:15:51 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,7 @@
 """ Matt Evans """
+
+password = input("Pick a password: ")
+while len(password)<6:
+    print("Password not long enough")
+    password = input("Pick another password: ")
+print('*'*len(password))
